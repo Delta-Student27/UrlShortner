8894b79483aa78cfcc4d5275f0aaa14ef976b805
Delta-Student27 <tanushripatil26@gmail.com>
2026-01-08 14:16:46 +0530

    Initial commit

---
+++ src/main/java/com/example/URLshortner/model/UrlMapping.java
@@ -0,0 +1,63 @@
+package com.example.URLshortner.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url_mapping")
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true )
+    private String shortCode;
+
+    private Long clickCount = 0L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public Long getClickCount() {
+        return clickCount;
+    }
+
+    public void setClickCount(Long clickCount) {
+        this.clickCount = clickCount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/example/URLshortner/repository/UrlRepository.java
@@ -0,0 +1,10 @@
+package com.example.URLshortner.repository;
+
+import com.example.URLshortner.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlMapping,Long> {
+    Optional<UrlMapping> findByShortCode(String shortCode);
+}
